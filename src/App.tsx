19604beca74c26b181697df71609ea19817d022b
Stefano Miceli <mic@nucleode.com>
2023-04-13 12:47:14 +0200

    adds ts react examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import ClassComponent from "./components/ClassComponent";
+import FunctionalComponent from "./components/FunctionalComponent";
+import Home from "./components/Home";
+import BootstrapElements from "./components/BootstrapElements";
+import FormComponent from "./components/FormComponent";
+import FetchComponent from "./components/FetchComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/class-component"
+              element={
+                <>
+                  <ClassComponent title="Ciao a tutti, sono un class component!" />
+                  <ClassComponent title="Ciao component!" subtitle="Epicode" />
+                </>
+              }
+            />
+            <Route
+              path="/functional/:id"
+              element={<FunctionalComponent title="Hello, I'm a functional component" subtitle="another thing here" />}
+            />
+            <Route path="/bootstrap-elements" element={<BootstrapElements />} />
+            <Route path="/form" element={<FormComponent />} />
+            <Route path="/fetch" element={<FetchComponent />} />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
